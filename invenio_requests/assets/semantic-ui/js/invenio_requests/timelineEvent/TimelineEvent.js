e9727fa630f7a829d6f61fe37c6cb136fb0c7194
pablo <pineirin@gmail.com>
2022-03-10 10:28:30 +0100

    Request details: add relative date to comment feed * closes inveniosoftware/invenio-requests#155

--- invenio_requests/assets/semantic-ui/js/invenio_requests/timelineEvent/TimelineEvent.js
+++ invenio_requests/assets/semantic-ui/js/invenio_requests/timelineEvent/TimelineEvent.js
@@ -4,22 +4,16 @@
 // Invenio RDM Records is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-import Error from '../components/Error';
-import FormattedInputEditor
-  from '../components/FormattedInputEditor';
+import Error from "../components/Error";
+import FormattedInputEditor from "../components/FormattedInputEditor";
 import React, { Component } from "react";
-import {
-  Feed,
-  Image,
-  Container,
-  Dropdown,
-  Grid,
-} from "semantic-ui-react";
+import { Feed, Image, Container, Dropdown, Grid } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import Overridable from "react-overridable";
 import { SaveButton, CancelButton } from "../components/Buttons";
 import { TimelineEventBody } from "./TimelineEventBody";
 import { i18next } from "@translations/invenio_requests/i18next";
+import { DateTime } from "luxon";
 
 class TimelineEvent extends Component {
   constructor(props) {
@@ -30,6 +24,8 @@ class TimelineEvent extends Component {
     this.state = {
       commentContent: event?.payload?.content,
     };
+    this.timestampToRelativeTime = (timestamp) =>
+      DateTime.fromISO(timestamp).setLocale(i18next.language).toRelative();
   }
 
   render() {
@@ -52,7 +48,11 @@ class TimelineEvent extends Component {
       <Overridable id="TimelineEvent.layout" event={event}>
         <Feed.Event>
           <Feed.Label>
-            <Image src="/static/images/square-placeholder.png" as={Image} rounded />
+            <Image
+              src="/static/images/square-placeholder.png"
+              as={Image}
+              rounded
+            />
           </Feed.Label>
           <Feed.Content>
             <Grid>
@@ -62,7 +62,9 @@ class TimelineEvent extends Component {
                     {/*TODO replace with event.icon and add a translated event description*/}
                     <Feed.User as="a">{event.created_by?.user}</Feed.User>{" "}
                     {i18next.t("commented")}
-                    <Feed.Date>{event.created}</Feed.Date>
+                    <Feed.Date>
+                      {this.timestampToRelativeTime(event.created)}
+                    </Feed.Date>
                   </Feed.Summary>
                 </Grid.Column>
                 <Grid.Column width={1}>
@@ -83,7 +85,7 @@ class TimelineEvent extends Component {
             </Grid>
 
             <Feed.Extra text className="timeline-event-body">
-              {error && <Error error={error}/>}
+              {error && <Error error={error} />}
 
               {isEditing ? (
                 <FormattedInputEditor
